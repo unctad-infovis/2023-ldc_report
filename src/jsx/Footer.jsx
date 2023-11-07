155938f68a051b1e47fb9d0b7a41dc59eab31e3c
Teemo Tebest <teemo.tebest@gmail.com>
2023-11-07 15:57:48 +0100

    Updated videos

--- src/jsx/Footer.jsx
+++ src/jsx/Footer.jsx
@@ -9,52 +9,50 @@ function Footer() {
         <div className="footer_elements">
           <div className="footer_element footer_element_1">
             <div className="footer_content anchor_videos" id="anchor_videos">
-              <div className="hidden">
-                <h3>Watch the videos</h3>
-                <div className="iframe_container youtube_iframe">
-                  <iframe src="https://www.youtube.com/embed/NRFEuRqoR7U" title="Least Developed Countries Report" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
-                </div>
-                <ul>
-                  <li>
-                    <strong>Watch:</strong>
-                    {' '}
-                    <a href="https://youtu.be/xMTaiLRO8fM" target="_blank" rel="noreferrer">Français</a>
-                    {', '}
-                    <a href="https://youtu.be/OIxBxLvDL0Y" target="_blank" rel="noreferrer">Español</a>
-                    {', '}
-                    <a href="https://youtu.be/A5mIjaECTBc" target="_blank" rel="noreferrer">العربية</a>
-                    {', '}
-                    <a href="https://youtu.be/DskeZaWUiCA" target="_blank" rel="noreferrer">简体中文</a>
-                    {', '}
-                    <a href="https://youtu.be/00iFOntZxG0" target="_blank" rel="noreferrer">Русский</a>
-                    {', '}
-                    <a href="https://youtu.be/0EN3au-p81M" target="_blank" rel="noreferrer">Português</a>
-                    {', '}
-                    <a href="https://youtu.be/4OOyOL0uGnQ" target="_blank" rel="noreferrer">Kiswahili</a>
-                  </li>
-                </ul>
-                <ul>
-                  <li>
-                    <strong>Download:</strong>
-                    {' '}
-                    <a href="https://vimeo.com/868447138" target="_blank" rel="noreferrer">English</a>
-                    {', '}
-                    <a href="https://vimeo.com/868447431" target="_blank" rel="noreferrer">Français</a>
-                    {', '}
-                    <a href="https://vimeo.com/868447676" target="_blank" rel="noreferrer">Español</a>
-                    {', '}
-                    <a href="https://vimeo.com/868447013" target="_blank" rel="noreferrer">العربية</a>
-                    {', '}
-                    <a href="https://vimeo.com/868447067" target="_blank" rel="noreferrer">简体中文</a>
-                    {', '}
-                    <a href="https://vimeo.com/868447615" target="_blank" rel="noreferrer">Русский</a>
-                    {', '}
-                    <a href="https://vimeo.com/868447562" target="_blank" rel="noreferrer">Português</a>
-                    {', '}
-                    <a href="https://vimeo.com/868447493" target="_blank" rel="noreferrer">Kiswahili</a>
-                  </li>
-                </ul>
+              <h3>Watch the videos</h3>
+              <div className="iframe_container youtube_iframe">
+                <iframe src="https://www.youtube.com/embed/ej-gfhwhEMU" title="Least Developed Countries Report" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
               </div>
+              <ul>
+                <li>
+                  <strong>Watch:</strong>
+                  {' '}
+                  <a href="https://youtu.be/S0hxSgwPJzI" target="_blank" rel="noreferrer">Français</a>
+                  {', '}
+                  <a href="https://youtu.be/wT3jeGNZL4U" target="_blank" rel="noreferrer">Español</a>
+                  {', '}
+                  <a href="https://youtu.be/6xd9hcN12jE" target="_blank" rel="noreferrer">العربية</a>
+                  {', '}
+                  <a href="https://youtu.be/S23axWwcQGU" target="_blank" rel="noreferrer">简体中文</a>
+                  {', '}
+                  <a href="https://youtu.be/cVROjgKKVjg" target="_blank" rel="noreferrer">Русский</a>
+                  {', '}
+                  <a href="https://youtu.be/VPpMz8m8H-w" target="_blank" rel="noreferrer">Português</a>
+                  {', '}
+                  <a href="https://youtu.be/-zLJsyCI8zQ" target="_blank" rel="noreferrer">Kiswahili</a>
+                </li>
+              </ul>
+              <ul>
+                <li>
+                  <strong>Download:</strong>
+                  {' '}
+                  <a href="https://vimeo.com/882069158" target="_blank" rel="noreferrer">English</a>
+                  {', '}
+                  <a href="https://vimeo.com/882069181" target="_blank" rel="noreferrer">Français</a>
+                  {', '}
+                  <a href="https://vimeo.com/882069030" target="_blank" rel="noreferrer">Español</a>
+                  {', '}
+                  <a href="https://vimeo.com/882069068" target="_blank" rel="noreferrer">العربية</a>
+                  {', '}
+                  <a href="https://vimeo.com/882069119" target="_blank" rel="noreferrer">简体中文</a>
+                  {', '}
+                  <a href="https://vimeo.com/882069276" target="_blank" rel="noreferrer">Русский</a>
+                  {', '}
+                  <a href="https://vimeo.com/882069246" target="_blank" rel="noreferrer">Português</a>
+                  {', '}
+                  <a href="https://vimeo.com/882069214" target="_blank" rel="noreferrer">Kiswahili</a>
+                </li>
+              </ul>
               <div className="hidden">
                 <h4>Watch the press conference</h4>
                 <div className="iframe_container youtube_iframe">
@@ -68,32 +66,36 @@ function Footer() {
                   </li>
                 </ul>
               </div>
-              <div className="hidden">
-                <h4>Interview </h4>
-                <div className="iframe_container youtube_iframe">
-                  <iframe src="https://www.youtube.com/embed/IbVRtayUJoo" title="Interview with Shamika N. Sirimanne, Director of UNCTAD's Technology and Logistics Division" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
-                </div>
-                <ul>
-                  <li>
-                    <strong>Download:</strong>
-                    {' '}
-                    <a href="https://vimeo.com/868637024" target="_blank" rel="noreferrer">Video</a>
-                  </li>
-                </ul>
+              <h4>The 3 ‘As’ of development finance: Rolf Traeger, chief of UNCTAD’s least developed countries section</h4>
+              <div className="iframe_container youtube_iframe">
+                <iframe src="https://www.youtube.com/embed/XGn8739VwJg" title="The 3 ‘As’ of development finance: Rolf Traeger, chief of UNCTAD’s least developed countries section" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
               </div>
-              <div className="hidden">
-                <h4>Interview</h4>
-                <div className="iframe_container youtube_iframe">
-                  <iframe src="https://www.youtube.com/embed/RD57SmYtYG4" title="Interview with Jan Hoffmann, Head of UNCTAD's Trade Logistics Branch" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
-                </div>
-                <ul>
-                  <li>
-                    <strong>Download:</strong>
-                    {' '}
-                    <a href="https://vimeo.com/868636911" target="_blank" rel="noreferrer">Video</a>
-                  </li>
-                </ul>
+              <ul>
+                <li>
+                  <strong>Download:</strong>
+                  {' '}
+                  <a href="https://vimeo.com/881699770" target="_blank" rel="noreferrer">English</a>
+                  {', '}
+                  <a href="https://vimeo.com/881700188" target="_blank" rel="noreferrer">Français</a>
+                  {', '}
+                  <a href="https://vimeo.com/881700773" target="_blank" rel="noreferrer">Português</a>
+                  {', '}
+                  <a href="https://vimeo.com/881701583" target="_blank" rel="noreferrer">clean version</a>
+                </li>
+              </ul>
+              <h4>The difference between climate and development finance: Stefan Csordas, UNCTAD economist</h4>
+              <div className="iframe_container youtube_iframe">
+                <iframe src="https://www.youtube.com/embed/3W12bu9VmQA" title="The difference between climate and development finance: Stefan Csordas, UNCTAD economist" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
               </div>
+              <ul>
+                <li>
+                  <strong>Download:</strong>
+                  {' '}
+                  <a href="https://vimeo.com/881700104" target="_blank" rel="noreferrer">Video</a>
+                  {', '}
+                  <a href="https://vimeo.com/881699971" target="_blank" rel="noreferrer">clean version</a>
+                </li>
+              </ul>
             </div>
             <div className="footer_content anchor_podcasts" id="anchor_podcasts">
               <h3>Podcasts</h3>
